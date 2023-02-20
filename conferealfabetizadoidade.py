36da00842d67bf58dc57587fa7739da680815087
LucasJRufino <Lucas.C.Rufino@hotmail.com>
2023-02-20 14:07:09 -0300

    feat: ensures the person is both above twenty-five years old and literate

---
+++ conferealfabetizadoidade.py
@@ -0,0 +1,9 @@
+## Escreva um código que verifique se uma pessoa é alfabetizada (sabe ler e escrever) e tem mais de 25 anos de idade.
+
+alfab = input("É alfabetizado?(S/N): ")
+idade = int(input("Qual a idade?"))
+
+if alfab == S and idade > 25:
+  print("A pessoa é elegível")
+else:
+  print("A pessoa não é elegível")
