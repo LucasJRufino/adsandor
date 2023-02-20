db41cfecec517f772d5cab00c818ccde8960914d
LucasJRufino <Lucas.C.Rufino@hotmail.com>
2023-02-20 14:05:47 -0300

    fix: should verify if its even and divisible by three

--- verificaparimpar.py
+++ verificaparimpar.py
@@ -1,6 +1,10 @@
+## Escreva um código que verifique se um número é par ou divisível por 3.
+
 num = int(input("Insira o número: "))
 
 if num%2 > 0:
   print("O número é impar")
+  if num%3 == 0:
+    print("O número é divisível por 3")
 else:
   print("O número é par")
