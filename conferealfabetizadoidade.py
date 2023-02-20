737dd7dee4b730b5fbf2e48ced2da4be1faf6d6f
LucasJRufino <Lucas.C.Rufino@hotmail.com>
2023-02-20 14:08:24 -0300

    fix: letter 'S' was undefined, should be string

--- conferealfabetizadoidade.py
+++ conferealfabetizadoidade.py
@@ -3,7 +3,7 @@
 alfab = input("É alfabetizado?(S/N): ")
 idade = int(input("Qual a idade?"))
 
-if alfab == S and idade > 25:
+if alfab == "S" and idade > 25:
   print("A pessoa é elegível")
 else:
   print("A pessoa não é elegível")
